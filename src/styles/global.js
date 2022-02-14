3d41b3b9b8deac29420dfe0fd72dcab7b4ac1cf7
Fernando Lara <fmaia8@hotmail.com>
2022-02-14 18:19:14 -0300

    Passando o import da fonte para o index.html para evitar alerta

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,7 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap');
   *, *::after, *::before {
     margin: 0;
     padding: 0;
