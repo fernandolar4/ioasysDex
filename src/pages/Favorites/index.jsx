bdfc4a0482fef7649d15ffc2277b8ac06bf7994a
Fernando Lara <fmaia8@hotmail.com>
2022-02-24 17:51:22 -0300

    Planejando e preparando para adicionar a lista de favoritos

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -3,6 +3,7 @@ import * as S from "./Favorites.style";
 import Header from "../../components/Header";
 import FavoriteButton from "../../components/FavoriteButton";
 import BackButton from "../../components/BackButton";
+import PokeCard from "../../components/PokeCard";
 
 const Favorites = () => {
   return (
@@ -14,9 +15,13 @@ const Favorites = () => {
         <FavoriteButton />
         <span></span>
       </S.Links>
+      {/* <span>
+        1 - SALVAR OS POKEMONS FAVORITOS NO LOCAL STATE 2 - RENDERIZAR ELES
+        NESSA TELA 3 - LIMITAR O LOCAL STATE À 12 POKEMONS FAV 4 - RENDEZERIZAR
+        OS BLOCOS CINZAS QUANDO NÃO HOUVER X POKEMONS FAV
+      </span> */}
       <div className="mobileOnlyButton">
-
-      <BackButton />
+        <BackButton />
       </div>
     </S.PageContainer>
   );
