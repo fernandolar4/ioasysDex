9fbca45d2295179a83c5645e3152dbb7ae1856a3
Fernando Lara <fmaia8@hotmail.com>
2022-03-01 17:13:22 -0300

    Primeira implementação da feature de busca

--- src/pages/Home/Home.style.js
+++ src/pages/Home/Home.style.js
@@ -21,3 +21,58 @@ export const Box = styled.div`
       
   }
 `;
+
+
+export const Formulario = styled.form`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid ${({ theme }) => theme.colors.Ioasys};
+
+  max-width: 630px;
+  border-radius: 10px;
+  padding: 14px;
+
+  @media (max-width: 900px) {
+    margin-left: 5%;
+    max-width: 80%;
+    padding: 16px 40px 15px 50px;
+  }
+  label {
+    color: ${({ theme }) => theme.colors.Ioasys};
+    background-color: ${({ theme }) => theme.colors.Background};
+    position: relative;
+    float: right;
+    font-size: 16px;
+    padding: 4px;
+    bottom: 35px;
+    right: 233px;
+    @media (max-width: 900px) {
+      float: none;
+      padding: 2px;
+      bottom: 30px;
+      right: 150px;
+    }
+  }
+  input {
+    border: 0;
+    font-size: 16px;
+    height: 27px;
+    margin: 0px 34px;
+    @media (max-width: 900px) {
+      height: 17px;
+      font-size: 14px;
+      margin: 0px 0px;
+    }
+
+    .lupa {
+      @media (max-width: 900px) {
+        margin-right: 100px;
+      }
+    }
+    :focus-visible {
+      outline: none;
+    }
+  }
+`;
+
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,18 +1,32 @@
 import * as S from "./Home.style";
 
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
 
 import Header from "../../components/Header";
-import Search from "../../components/Search";
+
 import FavoriteButton from "../../components/FavoriteButton";
 import PokeCard from "../../components/PokeCard";
 import getAllPokemons from "../../services/getAllPokemons";
+
+import Lupa from "../../assets/icons/magnifier.svg";
+import X from "../../assets/icons/x.svg";
 
 const Home = () => {
   const [pokemons, setPokemons] = useState([]);
   const [scrollEnds, setScrollEnds] = useState(false);
   const [nextPokemonsUrl, setNextPokemonsUrl] = useState("/pokemon");
 
+  const [inputPokemon, setInputPokemon] = useState("");
+
+  async function handleBuscaPkm() {
+    const data = await axios({
+      url: `https://pokeapi.co/api/v2/pokemon/${inputPokemon}`,
+    });
+    setPokemons([data]);
+  }
 
   window.onscroll = function (ev) {
     if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
@@ -33,11 +47,49 @@ const Home = () => {
     };
     handleGetAllPokemons();
   }, [scrollEnds]);
+
   return (
     <S.HomeContainer>
       <Header />
       <S.Box>
-        <Search  />
+        <S.Formulario
+          onSubmit={(event) => {
+            event.preventDefault();
+            handleBuscaPkm(inputPokemon);
+          }}
+        >
+          <input
+            type="text"
+            id="search"
+            placeholder="Buscar pokemon"
+            className={"textarea"}
+            value={inputPokemon}
+            onChange={(event) => {
+              setInputPokemon(event.target.value);
+              console.log(inputPokemon);
+            }}
+          />
+          <label htmlFor="search">Buscar</label>
+          {inputPokemon ? (
+            <input
+              to="/"
+              type="image"
+              src={X}
+              alt="Lupa"
+              className={"lupa"}
+              onClick={(event) => {
+                event.refresh();
+              }}
+            ></input>
+          ) : (
+            <input
+              type="image"
+              src={Lupa}
+              alt="Lupa"
+              className={"lupa"}
+            ></input>
+          )}
+        </S.Formulario>
         <FavoriteButton className={"nameless"} />
       </S.Box>
       <PokeCard pokemons={pokemons} />
